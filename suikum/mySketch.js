5794ffbfbce136ccbfd03a9aac12550ca85b87d9
Fabio Cosentino <32210256+Costa-exe@users.noreply.github.com>
2023-12-07 18:58:19 +0100

    Update mySketch.js

--- suikum/mySketch.js
+++ suikum/mySketch.js
@@ -43,8 +43,7 @@ function draw() {
 
     if (curr) {
         curr.sprite.y = 25
-        console.log(mouseX + ',' + 10 + curr.sprite.d / 2 + ',' + 490 - curr.sprite.d / 2);
-        curr.sprite.x = constrain(mouseX, 10 + curr.sprite.d / 2, 490 - curr.sprite.d / 2)
+        curr.sprite.x = constrain(Number(mouseX), 10 + Number(curr.sprite.d) / 2, 490 - Number(curr.sprite.d) / 2)
         curr.sprite.velocity.y = 0
     } else {
         timer++
